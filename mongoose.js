5d9c5c28a654601a9a0647ab26eebfff640627d4
letrungtiennbk9 <letrungtiennbk9@gmail.com>
2019-11-12 05:22:35 +0700

    ahiahia

--- mongoose.js
+++ mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost/test');
 
-let Book = mongoose.model('Book', {name: String});
-let practicalNodeBook = new Book({name:'Practical Node Book'});
+let Book = mongoose.model('Book', {name: String, price: String, type: String, img: String});
+let practicalNodeBook = new Book({name:'Practical Node Book', price:'10000', type: "Tay cầm", img:"a.jpg"});
 practicalNodeBook.save((err, results) => {
   if(err){
     console.log(err);
